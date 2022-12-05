33a365dc1de83f6360b3315fe01a5da49e33957f
rgu3st <robguest@gmail.com>
2022-12-05 14:57:56 -0600

    Starting day4.

---
+++ venv/Day4.py
@@ -0,0 +1,12 @@
+
+
+
+''' 'main' for Day4'''
+def run():
+    print("Running day 4.")
+
+    inputData = []
+
+    with open("./Data/input_day4.txt", 'r') as fileData:
+        inputData = fileData.readlines()
+        print(inputData)
--- venv/main.py
+++ venv/main.py
@@ -1,11 +1,13 @@
 import Day1 as d1
 import Day2 as d2
 import Day3 as d3
+import Day4 as d4
 
 def main():
     # d1.run_day_1()
     # d2.run()
-    d3.run()
+    #d3.run()
+    d4.run()
 
 if __name__ == "__main__":
     main()
